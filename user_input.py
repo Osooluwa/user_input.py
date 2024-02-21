3705087a06a8c74d59bcc068eb61c37a39bffdc3
Osooluwa Ajibola <105592203+Osooluwa@users.noreply.github.com>
2024-02-21 22:05:50 +0100

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Your location: ')
+
+print('Your name is {}. You are {} years and you live in {}.'.format(name, age, location))
